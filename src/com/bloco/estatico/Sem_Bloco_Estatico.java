1f603ddab3ef7e93c04add728aa622752b10798b
Vinicius Thiago <vtls@cin.ufpe.br>
2019-11-03 14:21:48 -0300

    alteraçao

--- src/com/bloco/estatico/Sem_Bloco_Estatico.java
+++ src/com/bloco/estatico/Sem_Bloco_Estatico.java
@@ -2,12 +2,15 @@ package com.bloco.estatico;
 
 public class Sem_Bloco_Estatico {
 
-	 static int x = 1;
+	 static int x = 4;
+	 static int z = 3;
      static int y = 2;
 	
-	public static void soma (int x,int y)
+	public static void soma (int x,int z)
 	{
-		System.out.println(x+y);
+		int a = x + z;
+		System.out.println(a+x);
+		System.out.println(x+z);
 	}
 	
 	public static void main(String[] args) 
